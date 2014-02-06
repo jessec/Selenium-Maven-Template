e6ce42e730acc73ee302fb67eb659714d56ab006
rcooke <rcooke@pc-rcooke2.trimm.local>
2014-02-06 14:41:47 +0100

    cc

---
+++ src/test/java/trimm/selenium/samples/simple/TestGoogleSearch.java
@@ -0,0 +1,14 @@
+package trimm.selenium.samples.simple;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGoogleSearch {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
